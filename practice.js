896eea8ea3b093ab1c821e540bed9617af80d1f2
luvhirani <luvhirani0@gmail.com>
2025-06-23 10:37:53 +0530

    second commit

--- practice.js
+++ practice.js
@@ -8,14 +8,37 @@
 // console.log("a defined ?", + (typeof a !== 'undefined')) //false
 // console.log("b defined ?", + (typeof b !== 'undefined')) //true
 
-var a = {} 
+// var a = {} 
 
-var b = {key:'b'}; 
+// var b = {key:'b'}; 
 
-var c = {key:'c'}; 
+// var c = {key:'c'}; 
 
-a[b]=123 
+// a[b]=123 
 
-b[c]=456
+// b[c]=456
 
-console.log(a[b]) 
+// console.log(a[b]) 
+
+// "use strict"
+
+// function ref (){
+//     console.log(this)
+// }
+
+// ref()
+
+// console.log(this)
+
+
+// var a
+
+
+// console.log(a)
+// var a = 10;
+
+
+(function sum (){
+    console.log("inside sum")
+}())
+
